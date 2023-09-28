a6252eb092008aebc4a303e941fd0528d90c4184
Jonathan Araujo [Jojo] <e_jaraujo@smu.cl>
2023-09-28 07:48:34 -0300

    Add getNews OnInit, Paginator, Search and Ordering

--- src/app/news/news-list/news-list.component.ts
+++ src/app/news/news-list/news-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { News } from '../news';
 import { NewsService } from '../news.service';
+import { PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-news-list',
@@ -8,17 +9,53 @@ import { NewsService } from '../news.service';
   styleUrls: ['./news-list.component.css']
 })
 export class NewsListComponent implements OnInit {
-  newsList$: News[] = []
+  newsList: News[] = []
+
+  search = '';
+  newsLength: number = 0
+  offset: number = 0
+  ordering: string = '-published_at'
 
   constructor(private newsServices: NewsService) {}
 
+
   ngOnInit(): void {
-    this.newsServices.getNews().subscribe((data) => {
-      this.newsList$ = data.results
-    });
+    this.newsServices.getNews(this.offset, this.ordering, this.search)
+      .subscribe((data) => {
+        this.newsList = data.results
+        this.newsLength = data.count
+      });
   }
 
   addNewsToFavorite(id: number) {
     console.log({ id })
+  }
+
+  handlePageEvent({ pageIndex, pageSize }: PageEvent) {
+    const newOffSet = pageIndex * pageSize
+
+    this.newsServices.getNews(newOffSet, this.ordering, this.search)
+      .subscribe((data) => {
+        this.newsList = data.results
+        this.offset = newOffSet
+      });
+  }
+
+  handleOrderEvent() {
+    const newOrdering = this.ordering === 'published_at' ? '-published_at' : 'published_at'
+
+    this.newsServices.getNews(this.offset, newOrdering, this.search)
+      .subscribe((data) => {
+        this.newsList = data.results
+        this.ordering = newOrdering
+      });
+  }
+
+  handleSearchEvent() {
+    this.newsServices.getNews(this.offset, this.ordering, this.search)
+      .subscribe((data) => {
+        this.newsList = data.results
+        this.newsLength = data.count
+      });
   }
 }
