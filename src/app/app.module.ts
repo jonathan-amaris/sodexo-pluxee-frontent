7f78463522b1a5c87f1721fb4d69ef4f8191d2e8
Jonathan Araujo [Jojo] <e_jaraujo@smu.cl>
2023-09-28 07:47:10 -0300

    Install Material and import some modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import {MatPaginatorModule} from '@angular/material/paginator';
+import { NgIf } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NewsListComponent } from './news/news-list/news-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -16,7 +24,15 @@ import { NewsListComponent } from './news/news-list/news-list.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    MatPaginatorModule
+    MatPaginatorModule,
+    MatButtonModule,
+    MatInputModule,
+    NgIf,
+    FormsModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatToolbarModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
