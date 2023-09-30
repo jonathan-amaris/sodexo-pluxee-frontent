4445de4c0e0ab327cb081058ca3961e3bac663bc
Jonathan Araujo [Jojo] <e_jaraujo@smu.cl>
2023-09-29 23:52:23 -0300

    Import MetModules and wrap a container

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,16 +9,20 @@ import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTableModule } from '@angular/material/table';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NewsListComponent } from './news/news-list/news-list.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FavoriteNewsComponent } from './news/favorite-news/favorite-news.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NewsListComponent
+    NewsListComponent,
+    FavoriteNewsComponent
   ],
   imports: [
     BrowserModule,
@@ -32,6 +36,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatIconModule,
     MatFormFieldModule,
     MatToolbarModule,
+    MatSnackBarModule,
+    MatTableModule,
     BrowserAnimationsModule
   ],
   providers: [],
